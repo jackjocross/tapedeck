c4b88a7734e4bc29f06bb3166339584f2aec3f2b
jcros8 <jjcross@umich.edu>
2017-01-29 21:50:17 -0500

    feat(linting): added client linting

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () =>
+  <div className="App">
+    <div className="App-header">
+      <h2>Welcome to Tapedeck</h2>
+    </div>
+    <p className="App-intro">
+      To get started, edit <code>src/App.js</code> and save to reload.
+    </p>
+  </div>;
+
+export default App;
