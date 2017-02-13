c4ba377c946236de6fdb2f0f5bd9efbdd74f0530
jcros8 <jjcross@umich.edu>
2017-02-12 23:17:47 -0500

    feat(cron) updated cron to UTC

--- server/cron/index.js
+++ server/cron/index.js
@@ -5,7 +5,7 @@ import create from '../loaders/create';
 import refreshToken from '../utils/refreshToken';
 
 export default function () {
-  cron.schedule('10 4 * * *', () => {
+  cron.schedule('10 9 * * *', () => {
     loadDb().then((tokens) => {
       tokens.forEach(({ id }) =>
         refreshToken(id).then(({ plugins }) =>
