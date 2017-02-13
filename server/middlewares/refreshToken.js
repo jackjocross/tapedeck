9507a8469cfcb9192dc925f198084026bfcc24d9
jcros8 <jjcross@umich.edu>
2017-02-12 22:20:22 -0500

    chore(package): pointed to git plugin

--- server/middlewares/refreshToken.js
+++ server/middlewares/refreshToken.js
@@ -1,5 +1,5 @@
 import { decrypt } from '../utils/crypto';
-import { refreshToken } from '../utils/refreshToken';
+import refreshToken from '../utils/refreshToken';
 
 export default function (req, res, next) {
   refreshToken(decrypt(req.cookies.tpdk)).then(() => next());
